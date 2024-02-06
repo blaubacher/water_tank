ebb739e788b656a33d05f3f4305a068b7c3261b8
blaubacher <brianlaubacher@gmail.com>
2024-02-06 15:22:52 -0700

    single module file

--- app/main.f90
+++ app/main.f90
@@ -1,54 +1,33 @@
 program main
-implicit none
-real, parameter :: g=9.81, pi=3.14159, rho_ref=1000.0
-real :: tank_diam=10.0, orifice_diam=1.0, h=40.0, sg=1.0
-real :: mass, vol, mdot, v, p, sw, rho, dm, csa
-real :: time=0, dt=1.0, output(500000,6)
-integer :: i=1, j
+  use water_tank, only : tank_t
+  implicit none
+  real :: time=0, dt=1.0, output(500000,6), dm
+  type(tank_t) :: mytank
+  integer :: i=1, j
 
-csa=pi*(tank_diam/2)**2
+  mytank=tank_t(diam=10, orifice_diam=1.0, h=40, sg=1.0)
 
-! compute the specific weight of the fluid
-rho=rho_ref*sg
-sw=rho*g
-vol=csa*h
-mass=vol*rho
-p=sw*h
-v=sqrt(2*p/rho)
-
-! code is intialized, now proceed to produce the results
-do while(h>5.0)
-  ! output the required information at start of time step
-  output(i,:)=[time, mass, h, p, v, mdot]
-! compute pressure
-  p=sw*h
-  ! compute velocity
-  v=sqrt(2*p/rho)
-  ! compute the mass flow rate
-  associate(a=> pi/4.0*orifice_diam**2)
-    mdot=rho*v*a
-  end associate
+  do while(mytank%h>=5.0)
   
-  ! compute mass increment
-  dm=mdot*dt
-  ! update tank conditions
-  mass=mass-dm
+    output(i,:)=[time, mytank%mass(), mytank%h, mytank%p(), &
+                mytank%vel(), mytank%mdot()]
 
-  h=mass/rho/csa
- 
+    ! compute the change in tank height based on 
+    ! outflow of a given mass
+    dm=mytank%mdot()*dt
+    mytank%h=mytank%h-dm/(mytank%rho()*mytank%csa())
 
-  ! update counters and time
-  i=i+1
-  time=time+dt
-end do
+    i=i+1
+    time=time+dt
+  end do
 
-! write output to a file
-open(unit=15, file='outputdat', form='formatted', status='unknown')
-do j=1,i-1
-  write(15,'(7f15.3)') output(j,:)
-end do
-close(15)
-print *, 'all done folks!'
+  ! write output to a file
+  open(unit=15, file='outputdat', form='formatted', status='unknown')
+  do j=1,i-1
+    write(15,'(7f15.3)') output(j,:)
+  end do
+  close(15)
+  print *, 'all done folks!'
 
 ! close program
 
--- src/water_tank.f90
+++ src/water_tank.f90
@@ -1,10 +1,61 @@
 module water_tank
   implicit none
   private
+  real, parameter :: rho_ref=1000.0, pi=3.14159, g=9.81
 
-  public :: say_hello
-contains
-  subroutine say_hello
-    print *, "Hello, water_tank!"
-  end subroutine say_hello
+  type, public :: tank_t
+    real:: diam, orifice_diam, h, sg
+  contains  
+    procedure :: mass, mdot, vel, p, rho, csa, sw, oa
+  end type tank_t
+
+  contains
+
+  module function mass(tank)
+    class(tank_t), intent(in) :: tank
+    real :: mass
+        mass=tank%rho()*tank%csa()*tank%h
+  end function
+
+  module function p(tank)
+    class(tank_t), intent(in) :: tank
+    real :: p
+    p=tank%sw()*tank%h
+  end function
+
+  module function vel(tank)
+    class(tank_t), intent(in) :: tank
+    real :: vel
+    vel=sqrt(2*tank%p()/tank%rho())
+  end function
+
+  module function mdot(tank)
+    class(tank_t), intent(in) :: tank
+    real :: mdot
+    mdot=tank%rho()*tank%vel()*tank%oa()
+  end function
+
+  module function sw(tank)
+    class(tank_t), intent(in) :: tank
+    real :: sw
+    sw=tank%rho()*g
+  end function
+
+  module function rho(tank)
+    class(tank_t), intent(in) :: tank
+    real :: rho
+    rho=tank%sg*rho_ref
+  end function
+
+  module function csa(tank)
+    class(tank_t), intent(in) :: tank
+    real :: csa
+    csa=pi*(tank%diam/2)**2
+  end function
+
+  module function oa(tank)
+    class(tank_t), intent(in) :: tank
+    real :: oa
+    oa=pi*(tank%orifice_diam/2)**2
+  end function
 end module water_tank
